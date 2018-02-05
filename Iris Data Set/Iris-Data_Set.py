02ca819f5480c418debec2afdb2e319e16bddb12
Vaishak V K <vaishakhvkv@gmail.com>
2018-02-05 13:53:13 +0530

    Update Iris-Data_Set.py

--- Iris Data Set/Iris-Data_Set.py
+++ Iris Data Set/Iris-Data_Set.py
@@ -3,3 +3,10 @@
 #Dependencies
 import pandas as pd
 import matplotlib as mp
+
+# Create Scattermatrix plot to check the relationships
+#Remove the erroneous classes
+# Remove outliers
+# Remove NA values
+# Check for Scatter plot to ensure no outliers are available.
+# Classification and Parmater tuning. 
